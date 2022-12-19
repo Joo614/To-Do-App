c37a84173ac0556a9212e987b17b4174a7d69200
Joo614 <57wnguswn57@gmail.com>
2022-12-19 22:27:59 +0900

    api 문서화 ~ing

--- src/main/java/com/project1/todo1/Todo1Application.java
+++ src/main/java/com/project1/todo1/Todo1Application.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Todo1Application extends SpringBootServletInitializer {
+public class Todo1Application { //extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Todo1Application.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Todo1Application.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(Todo1Application.class);
+//	}
 }
--- src/test/java/com/project1/todo1/controllerTest/TodoControllerMockTest.java
+++ src/test/java/com/project1/todo1/controllerTest/TodoControllerMockTest.java
@@ -9,11 +9,13 @@ import com.project1.todo1.todo.service.TodoService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,13 +24,17 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @MockBean(JpaMetamodelMappingContext.class)
 @WebMvcTest(TodoController.class)
-@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class TodoControllerMockTest {
     @Autowired
     private MockMvc mockMvc;
@@ -48,7 +54,6 @@ public class TodoControllerMockTest {
         TodoDto.Post post = new TodoDto.Post("studying",1,false);
         TodoDto.Response response = new TodoDto.Response(1,"studying",1,false);
 
-        //TodoEntity todo = mapper.todoPostToTodo(post);
         given(todoService.createTodo(Mockito.any())).willReturn(new TodoEntity());
         given(mapper.todoToTodoResponse(Mockito.any(TodoEntity.class))).willReturn(response);
 
@@ -62,43 +67,82 @@ public class TodoControllerMockTest {
                         .content(content));
 
         // then
-        MvcResult result = actions
-                .andExpect(status().isCreated())
+        actions.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.title").value(post.getTitle()))
                 .andExpect(jsonPath("$.order").value(post.getOrder()))
                 .andExpect(jsonPath("$.completed").value(post.isCompleted()))
-                .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
+                .andDo(document("post-todo",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("해야 할 일"),
+                                        fieldWithPath("order").type(JsonFieldType.NUMBER).description("등록 순서").optional(),
+                                        fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("todoId").type(JsonFieldType.NUMBER).description("Todo 식별자"),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("해야 할 일"),
+                                        fieldWithPath("order").type(JsonFieldType.NUMBER).description("등록 순서"),
+                                        fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
+                                )
+                        )
+                ));
     }
 
     @Test
     void patchTodoTest() throws Exception {
         // given
+        long todoId = 1;
+
         TodoDto.Patch patch = new TodoDto.Patch(1, "sleeping", 1, false);
+        String content = gson.toJson(patch);
+
         TodoDto.Response response = new TodoDto.Response(1, "sleeping", 1, false);
 
         given(mapper.todoPatchToTodo(Mockito.any(TodoDto.Patch.class))).willReturn(new TodoEntity());
         given(todoService.updateTodo(Mockito.any(TodoEntity.class))).willReturn(new TodoEntity());
         given(mapper.todoToTodoResponse(Mockito.any(TodoEntity.class))).willReturn(response);
 
-        String content = gson.toJson(patch);
-
         // when
         ResultActions actions =
-                mockMvc.perform(patch("/todos/" + patch.getTodoId())
+                mockMvc.perform(
+                        patch("/todos/{todo-id}", todoId)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content));
 
         // then
-        MvcResult result = actions.andExpect(status().isOk())
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.todoId").value(patch.getTodoId()))
                 .andExpect(jsonPath("$.title").value(patch.getTitle()))
                 .andExpect(jsonPath("$.order").value(patch.getOrder()))
                 .andExpect(jsonPath("$.completed").value(patch.isCompleted()))
-                .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
+                .andDo(document("patch-todo",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("todo-id").description("Todo 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("todoId").type(JsonFieldType.NUMBER).description("Todo 식별자"),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("해야 할 일").optional(),
+                                        fieldWithPath("order").type(JsonFieldType.NUMBER).description("등록 순서").optional(),
+                                        fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("todoId").type(JsonFieldType.NUMBER).description("Todo 식별자"),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("해야 할 일"),
+                                        fieldWithPath("order").type(JsonFieldType.NUMBER).description("등록 순서"),
+                                        fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
+                                )
+                        )
+                ));
     }
 
 
@@ -114,17 +158,29 @@ public class TodoControllerMockTest {
         given(mapper.todoToTodoResponse(Mockito.any(TodoEntity.class))).willReturn(response);
 
         // when
-        ResultActions actions = mockMvc.perform(get("/todos/" + todo.getTodoId())
+        ResultActions actions = mockMvc.perform(get("/todos/{todo-id}", todo.getTodoId())
                 .accept(MediaType.APPLICATION_JSON));
 
         // then
-        MvcResult result = actions.andExpect(status().isOk())
+        actions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.title").value(todo.getTitle()))
                 .andExpect(jsonPath("$.order").value(todo.getOrder()))
                 .andExpect(jsonPath("$.completed").value(todo.isCompleted()))
-                .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
+                .andDo(document("get-todo",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("todo-id").description("Todo 식별자")
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("todoId").type(JsonFieldType.NUMBER).description("Todo 식별자"),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("해야 할 일"),
+                                        fieldWithPath("order").type(JsonFieldType.NUMBER).description("등록 순서"),
+                                        fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
+                                )
+                        )
+                ));
     }
 
     @Test
@@ -148,7 +204,7 @@ public class TodoControllerMockTest {
                 .accept(MediaType.APPLICATION_JSON));
 
         // then
-        MvcResult result = actions.andExpect(status().isOk())
+        actions.andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$.[0].title").value(todo1.getTitle()))
                 .andExpect(jsonPath("$.[0].order").value(todo1.getOrder()))
@@ -156,9 +212,19 @@ public class TodoControllerMockTest {
                 .andExpect(jsonPath("$.[1].title").value(todo2.getTitle()))
                 .andExpect(jsonPath("$.[1].order").value(todo2.getOrder()))
                 .andExpect(jsonPath("$.[1].completed").value(todo2.isCompleted()))
-                .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
+                .andDo(document("get-todos",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("[]").type(JsonFieldType.ARRAY).description("결과 리스트"),
+                                        fieldWithPath("[].todoId").type(JsonFieldType.NUMBER).description("Todo 식별자"),
+                                        fieldWithPath("[].title").type(JsonFieldType.STRING).description("해야 할 일"),
+                                        fieldWithPath("[].order").type(JsonFieldType.NUMBER).description("등록 순서"),
+                                        fieldWithPath("[].completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
+                                )
+                        )
+                ));
     }
 
     @Test
@@ -169,7 +235,7 @@ public class TodoControllerMockTest {
         doNothing().when(todoService).deleteTodo(todoId);
 
         // when
-        ResultActions actions = mockMvc.perform(delete("/todos/" + todoId));
+        ResultActions actions = mockMvc.perform(delete("/todos/{todo-id}", todoId));
 
         // then
         actions.andExpect(status().isNoContent());
